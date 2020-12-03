d18946de2162b6b3fac2f941723a31f9c34beb49
Emanuele Di marco <edi-marc@e3r3p8.42roma.it>
2020-12-03 14:47:53 +0100

    all exercises added + moulinette

---
+++ C_01/ex08/ft_sort_int_tab.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edi-marc <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/03 08:16:57 by edi-marc          #+#    #+#             */
+/*   Updated: 2020/12/03 09:12:43 by edi-marc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_swap(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int swap;
+	int i;
+
+	swap = 1;
+	while (swap == 1)
+	{
+		swap = 0;
+		i = 0;
+		while (i < size - 1)
+		{
+			if (tab[i] > tab[i + 1])
+			{
+				ft_swap(&tab[i], &tab[i + 1]);
+				swap = 1;
+			}
+			i++;
+		}
+	}
+}
